781f6d3bd677ed5fe76f1e658344c34b977b9c17
21 Chapel <40070531+21chapel@users.noreply.github.com>
2020-07-31 07:03:58 -0400

    connecting to server with php
    1) install xampp
    there are settings to overcome to get mysql working, esp if you've installed workbench on windows. i had to some files in htdocs/data folder. and change the port from 3600
    
    2) install forticlient
    ping encs.concordia.ca in cmd, if you're not getting a response, login to the concordia vpn with forticlient

--- jobs.php
+++ jobs.php
@@ -1,36 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Jobs!</title>
-</head>
-<body>
 
 <?php
 $servername = "iyc353.encs.concordia.ca";
 $username = "iyc353_1";
 $password = "folklore";
-$dbname = "iyc353_1";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=iyc353_1", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "success?";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 
-$sql = "SELECT * FROM goal";
-$result = $conn->query($sql);
+$query= $conn->prepare("SELECT * FROM author");
+$query->execute();
+$result = $query->fetchAll();
+var_dump($result);
 
-if ($result->num_rows > 0) {
-while($row = $result->fetch_assoc()) {
-echo "id: " . $row["author"]. "<br>";
-}
-} else {
-echo "0 results";
-}
-$conn->close();
 ?>
-
-</body>
-</html>
