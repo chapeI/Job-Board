e844d294bfde419972a2cff6581b2ccdcc2c70cb
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-08-01 00:18:00 -0400

    respect key length restrictions in create table address statement

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -36,12 +36,12 @@ CREATE TABLE users (
 -- Optional user address information (employers should be required to have at least one entry)
 CREATE TABLE address (
     user_id BIGINT,
-    street VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    country VARCHAR(255),
-    postal_code VARCHAR(255),
-    designation VARCHAR(255),
+    street VARCHAR(127),
+    city VARCHAR(127),
+    state VARCHAR(127),
+    country VARCHAR(127),
+    postal_code VARCHAR(127),
+    designation VARCHAR(127),
     PRIMARY KEY (user_id, street, city, state, country, postal_code),
     FOREIGN KEY (user_id)
         REFERENCES users (user_id)
