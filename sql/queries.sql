bb49296fd58519118e16f05030ce6eda7f5bd9a1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-08-09 19:46:38 -0400

    general housekeeping

--- sql/queries.sql
+++ sql/queries.sql
@@ -2,7 +2,7 @@
 
 -----------------------------------------------------------------------------------------------------------------------
 
--- create use (see utilities)
+-- create user (see utilities)
 
 -- create employer, given id, name, and tier
 INSERT INTO employer (employer_id, employer_name, employer_tier)
@@ -12,7 +12,7 @@ INSERT INTO employer (employer_id, employer_name, employer_tier)
 
 -- delete employer, given id
 UPDATE users
-SET password = password * -1 -- cascades to employer; categories, postings, and applications cannot be resolved
+SET user_id = user_id * -1 -- cascades to employer; categories, postings, and applications cannot be resolved
 WHERE user_id = <id>;
 
 -----------------------------------------------------------------------------------------------------------------------
@@ -26,11 +26,14 @@ WHERE employer_id = <id>;
 
 -- report employer information, given id
 SELECT employer_name, description, email
-FROM employer, users
+FROM employer JOIN users ON employer_id = user_id
 WHERE employer_id = <id>;
-    AND employer_id = user_id
 
 --report employer address(es) and phone number(s) (see utilities)
+
+SELECT employer_name, description, email
+FROM employer JOIN users ON employer_id = user_id
+WHERE employer_id = 11;
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -93,8 +96,9 @@ WHERE employer_id = <employer id>
 -- report applicant information given employer id and posting id
 SELECT first_name, last_name, application_time, offer_time, close_time
 FROM job_seeker, application, posting
-WHERE employer_id = <employer_id>
-    AND posting_id = <posting_id>
+WHERE posting.employer_id = <employer id>
+    AND posting.posting_id = <posting id>
+    AND job_seeker.job_seeker_id = application.job_seeker_id
     AND application.employer_id = posting.employer_id
     AND application.posting_id = posting.posting_id;
 
@@ -103,38 +107,37 @@ WHERE employer_id = <employer_id>
 -- vi ------------------------------------------------------------------------------------------------------------------
 
 -- report posted jobs, given employer id, origin time, and bound time
-SELECT title, description, posting_time, number_of_openings,
-    COUNT( SELECT job_seeker_id, application_id
-        FROM application) AS number_of_applicants,
-    COUNT(SELECT job_seeker_id, application_id
-        FROM application
-        WHERE application_time IS '1000-01-01 00:00:00' -- sentinel for accepted offer
-            AND offer_time IS NOT NULL) AS number_of_accepted_offers
-FROM posting, application
+SELECT title, description, posting_time, number_of_openings, number_of_applicants, number_of_accepted_offers
+FROM posting, application,
+    (SELECT COUNT(job_seeker_id) AS number_of_applicants
+        FROM (SELECT job_seeker_id, application_id
+            FROM application
+            WHERE application.employer_id = <employer id>) AS a) AS b,
+    (SELECT COUNT(job_seeker_id) AS number_of_accepted_offers
+        FROM (SELECT job_seeker_id, application_id
+            FROM application
+            WHERE application_time = '1000-01-01 00:00:00') AS c) AS d -- sentinel for accepted offer
 WHERE posting.employer_id = <employer id>
     AND application.employer_id = <employer id>
     AND posting.posting_id = application.posting_id
-    AND posting_time BETWEEN <origin> AND <bound>
-GROUP BY title, description, posting_time, number_of_applicants;
+    AND posting_time BETWEEN <origin> AND <bound>;
 
 -----------------------------------------------------------------------------------------------------------------------
 
 -- vii ----------------------------------------------------------------------------------------------------------------
 
--- create user given email and password
+-- create user (see utilities)
 
--- create job-seeker, given first name, last name, and tier
-INSERT INTO job_seeker (first_name, last_name, job_seeker_tier)
-    VALUES (<fname>, <lname>, <tier>);
+-- create job-seeker, given id, first name, last name, and tier
+INSERT INTO job_seeker (job_seeker_id, first_name, last_name, job_seeker_tier)
+    VALUES (<id>, <fname>, <lname>, <tier>);
 
 -----------------------------------------------------------------------------------------------------------------------
 
--- create use (see utilities)
-
 -- delete job-seeker, given id
 UPDATE users
-SET password = password * -1 -- cascades to job-seeker; applications cannot be resolved
-WHERE user_id = <id>
+SET user_id = user_id * -1 -- cascades to job-seeker; applications cannot be resolved
+WHERE user_id = <id>;
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -147,9 +150,8 @@ WHERE job_seeker_id = <id>;
 
 -- report job-seeker information, given id
 SELECT first_name, last_name, description, email
-FROM job_seeker, users
-WHERE job_seeker_id = users_id
-    AND job_seeker_id = <id>;
+FROM job_seeker JOIN users ON job_seeker_id = user_id
+WHERE job_seeker_id = <id>;
 
 -- report job-seeker address(es) and phone number(s) (see utilities)
 
@@ -158,24 +160,24 @@ WHERE job_seeker_id = users_id
 -- viii ---------------------------------------------------------------------------------------------------------------
 
 -- search for jobs assuming input is employer name
-SELECT posting_time, close_time, title, description, number_of_openings
+SELECT posting_time, close_time, title, posting.description, number_of_openings
 FROM posting, employer
-WHERE employer_name = <input>
-    AND posting.employer_id = employer.employer_id;
+WHERE employer_name LIKE <%input%>
+    AND posting.employer_id = employer.employer_id
+    AND posting.employer_id > 0; -- to exclude postings from deleted employees
 
 -- search for jobs assuming input is employer category
 SELECT posting_time, close_time, title, description, number_of_openings
 FROM posting, employer_category
-WHERE category = <input>
-    AND posting.employer_id = employer_category.employer_id;
+WHERE category LIKE <%input%>
+    AND posting.employer_id = employer_category.employer_id
+    AND posting.employer_id > 0;
 
 -- search for jobs assuming input is a substring of posting title or description
 SELECT posting_time, close_time, title, description, number_of_openings
 FROM posting
-WHERE title LIKE '%{input}%' OR description LIKE '%{input}%'
-    AND employer_id IN ( -- to exclude postings from deleted employers
-        SELECT employer_id
-        FROM employer);
+WHERE title LIKE <%input%> OR description LIKE <%input%>
+    AND employer_id > 0;
 
 -- add record to search table, given id and input
 INSERT INTO search (user_id, input)
@@ -257,9 +259,9 @@ WHERE job_seeker_id = <job-seeker id>
 
 -- xiv & xv -----------------------------------------------------------------------------------------------------------
 
--- add a payment method, given user id, method id
+-- add a payment method, given user id, method id, and account number
 INSERT INTO payment_method (user_id, method_id)
-    VALUES (<id>, <method id>);
+    VALUES (<id>, <method id>, <account>);
 
 -- if automatic payments are selected
 UPDATE payment_method
@@ -309,7 +311,7 @@ WHERE user_id = <id>;
 SELECT employer_name, email, frozen_time AS status, balance
 FROM employer, users, user_balance
 WHERE employer_id = users.user_id
-    AND users.user_id = balance.user_id;
+    AND users.user_id = user_balance.user_id;
 
 -- report employer categories
 SELECT *
@@ -348,250 +350,23 @@ INSERT INTO users (email, password)
     VALUES (<email>, <password>);
 
 -- report address(es), given id
-SELECT street_number, street_name, city, state, country, postal_code, designation
+SELECT street_number, street_name, city, state, country, designation
 FROM address
 WHERE user_id = <id>;
 
 -- report phone number(s), given id
 SELECT phone_number, designation
-FROM phone
+FROM telephone
 WHERE user_id = <id>;
 
 -- virtual balances table
-CREATE VIEW user_balance
-    SELECT payment.user_id, SUM(payment_amount) - SUM(bill_amount) AS balance
-    FROM payment, bill
-    WHERE payment.user_id = bill.user_id;
-
-
--- --------------------------------------------------------------------------------------------------------------------------------
-
-** ABDUL'S QUERIES **
-
-#i
--- CREATING AN EMPLOYER
-INSERT INTO users (user_id, email, password)
-    VALUES (11, 'agohier01@booking.com', 'xpnTTJ81');
-    
-INSERT INTO employer (employer_id, employer_name, description, employer_tier)
-	VALUES (11, 'Mike', 'Front_end developer', '0');
-    
--- EDITING AN EMPLOYER INFO
-UPDATE employer
-SET description='Full Stack Developer'
-WHERE employer_id=11;
-
--- DISPLAYING AN EMPLOYER
-SELECT *
-FROM employer
-WHERE employer_id=11;
-
--- DELETING AN EMPLOYER
-DELETE FROM users
-WHERE user_id=11;
-
-DELETE FROM employer
-WHERE employer_id=11;
-
--- -----------------------------------------------------------------------
-#ii 
--- CREATING A CATEGORY BY AN EMPLOYER
-INSERT INTO employer (employer_id, employer_name, description, employer_tier)
-	VALUES (6, 'Mike', 'Front_end developer', '0');
-INSERT INTO employer_category(employer_id, category)
-	VALUES (6, 'IT');
-    
--- EDITING A CATEGORY BY AN EMPLOYER
-UPDATE employer_category
-SET category='Tech'
-WHERE employer_id=6;
-
--- DISPLAYING A CATEGORY BY AN EMPLOYER
-SELECT *
-FROM employer_category
-WHERE employer_id=6;
-
--- DELETING A CATEGORY BY AN EMPLOYER
-DELETE FROM employer_category
-WHERE employer_id=6;
--- -----------------------------------------------------------------------
-#iii
--- POSTING A NEW JOB BY AN EMPLOYER
-
-INSERT INTO posting(employer_id, posting_id, title, description, number_of_openings)
-	VALUES (1, 4, 'Construction worker', 'Infrastructure speaclist', 2);
-    
--- -----------------------------------------------------------------------
-#iv
--- Providing a job offer for an employee by an employer.
-
-UPDATE application
-SET offer_time = CURRENT_TIMESTAMP
-WHERE application_id = 1 AND job_seeker_id = 6
-    AND employer_id=1;
-
--- -----------------------------------------------------------------------
-#v 
--- Report of a posted job by an employer 
-
-SELECT posting.title, posting.description, date_posted, app.job_seeker_id, status
-FROM posting p, application app
-WHERE  p.posting_id=app.posting_id AND
-p.posting_id=1 AND employer_id=1;
-
--- -----------------------------------------------------------------------
-#vi 
--- Report of posted jobs by an employer during a specific period of time (Job
--- title, date posted, short description of the job up to 50 characters, number
--- of needed employees to the post, number of applied jobs to the post,
--- number of accepted offers).
-
-SELECT posting.title, posting.date_posted, posting.description, posting.number_of_openings,
- COUNT(application.application_id), COUNT(application.status)
-FROM posting p, application app
-WHERE p.posting_id=app.posting_id AND p.employer_id=1 AND
- date_posted BETWEEN '2019-01-01' AND CURDATE() AND app.status='accepted';
-
--- -----------------------------------------------------------------------
-#vii 
--- CREATING AN EMPLOYEE
-
-INSERT INTO users (user_id, email, password)
-    VALUES (12, 'agohier011@booking.com', 'xpnTTJ811');
-    
-INSERT INTO job_seeker (job_seeker_id, first_name, last_name, description, job_seeker_tier)
-	VALUES (12, 'Jen', 'Smith', 'receptionist', 0);
-    
--- EDITING AN EMPLOYEE INFO
-UPDATE job_seeker
-SET description='Babysitting'
-WHERE job_seeker_id=12;
-
--- DISPLAYING AN EMPLOYEE
-SELECT *
-FROM job_seeker
-WHERE job_seeker_id=12;
-
--- DELETING AN EMPLOYEE
-DELETE FROM users
-WHERE user_id=12;
-
-DELETE FROM job_seeker
-WHERE job_seeker_id=12;
-
--- -----------------------------------------------------------------------
-#viii
--- Search for a job by an employee.
-
-INSERT INTO search(search_id, user_id, input)
-	VALUES(7, 10, 'software developer');
-
-INSERT INTO job_seeker_search(job_seeker_id, search_id)
-	VALUES(10, 7);
-    
--- -----------------------------------------------------------------------
-#ix
--- Apply for a job by an employee
-
-INSERT INTO application(job_seeker_id, application_id, employer_id, posting_id)
-	VALUES(8, 5, 3, 2);
-     
- -- -----------------------------------------------------------------------
- #X
- -- Accept/Deny a job offer by an employee
- 
- UPDATE application
- SET status='accepted'
- WHERE job_seeker_id=6 AND application.application_id=1;
- 
- -- -----------------------------------------------------------------------
-#xi
--- Withdraw from an applied job by an employee
-
-UPDATE application
- SET status='withdrawn'
- WHERE job_seeker_id=6 AND application.application_id=1;
- 
--- -----------------------------------------------------------------------
-#xii
--- Delete a profile by an employee
-
-DELETE FROM application
-WHERE job_seeker_id=6 AND application.application_id=1;
-
--- -----------------------------------------------------------------------
-#xiii
-
--- Report of applied jobs by an employee during a specific period of time
--- (Job title, date applied, short description of the job up to 50 characters,
--- status of the application).
-
-
-SELECT posting.title, posting.date_posted, posting.description, application.status
-FROM posting p, application app
-WHERE p.posting_id=app.posting_id AND app.job_seeker_id=6 AND
- date_posted BETWEEN '2019-01-01' AND CURDATE();
-
--- -----------------------------------------------------------------------
-#xiv
--- Add/Delete/Edit a method of payment by a user
-
--- Adding another method of payment
-INSERT INTO payment_method (user_id, method_id, account_number)
-    VALUES (1, 2, '34-175-9681');
-
--- EDITING A METHOD OF PAYMENT
-UPDATE payment_method
-SET account_number='24-175-9681'
-WHERE user_id=1 AND method_id=2;
-
--- DELETING A METHOD OF PAYMENT
-DELETE FROM payment_method
-WHERE user_id=1 AND method_id=2;
-
--- -----------------------------------------------------------------------
-#xv
--- Add/Delete/Edit an automatic payment by a user.
-
--- ADDING AND EDITING AN AUTOMATIC PAYMENT
-UPDATE payment_method
-SET is_automatic=TRUE 
-WHERE user_id=1 AND method_id=1;
-
--- DELETING AN AUTOMATIC PAYMENT
-UPDATE payment_method
-SET is_automatic=FALSE 
-WHERE user_id=1 AND method_id=1;
-
--- -----------------------------------------------------------------------
-#xvi
--- Make a manual payment by a user
-
-INSERT INTO payment (payment_id, user_id, payment_amount)
-    VALUES (9, 4, 1, 20);
-
--- -----------------------------------------------------------------------
-#xvii
--- Report of all users by the administrator for employers or employees
--- (Name, email, category, status, balance)
-
-SELECT 'employer' AS employer, user_id, employer_name, email, employer_tier, SUM(bill_amount)
-FROM users u, employer e, bill b
-WHERE u.user_id=e.employer_id AND u.user_id=b.user_id
-UNION ALL
-SELECT 'job_seeker' AS job_seeker, user_id, first_name, last_name, email, job_seeker_tier, SUM(bill_amount)
-FROM users u, job_seeker j, bill b
-WHERE u.user_id=j.job_seeker_id AND u.user_id=b.user_id;
-
--- -----------------------------------------------------------------------
-#xviii
--- Report of all outstanding balance accounts (User name, email, balance,
--- since when the account is suffering).
-
-SELECT 'employer' AS employer, employer_name, email, SUM(bill_amount), frozen_time
-FROM users u, bill b, employer e
-WHERE u.user_id=e.employer_id AND u.user_id=b.user_id AND SUM(bill_amount)>0
-UNION ALL
-SELECT 'job_seeker' AS job_seeker, first_name, last_name, email, SUM(bill_amount), frozen_time
-FROM users u, bill b, job_seeker j
-WHERE u.user_id=j.job_seeker_id AND u.user_id=b.user_id AND SUM(bill_amount)>0;
+CREATE VIEW user_balance AS
+    SELECT a.user_id, total_payments - total_billed AS balance
+    FROM (SELECT user_id, SUM(payment_amount) AS total_payments
+        FROM payment
+        GROUP BY user_id) AS a,
+        (SELECT user_id, SUM(bill_amount) AS total_billed
+        FROM bill
+        GROUP BY user_id) AS b
+    WHERE a.user_id = b.user_id
+    GROUP BY a.user_id;
