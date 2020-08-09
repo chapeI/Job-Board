85b0311d020d37bf357587f4d4fb35240c0ef7a8
21 Chapel <40070531+21chapel@users.noreply.github.com>
2020-08-09 10:30:24 -0400

    reorganized code to remove redundant code

---
+++ employer/has/navbar.php
@@ -0,0 +1,95 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Employer View</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Interface
+    </div>
+
+
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseJobSeeker1" aria-expanded="true" aria-controls="collapseJobSeeker1">
+            <i class="fas fa-fw fa-graduation-cap"></i>
+            <span>Job Seeker</span>
+        </a>
+        <div id="collapseJobSeeker1" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Job Seeker Header</h6>
+                <a class="collapse-item" href="buttons.html">My Applications</a>
+                <a class="collapse-item" href="buttons.html">Interviews</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseEmployer" aria-expanded="true" aria-controls="collapseEmployer">
+            <i class="fas fa-fw fa-suitcase"></i>
+            <span>Employer</span>
+        </a>
+        <div id="collapseEmployer" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Employer Header</h6>
+                <a class="collapse-item" href="">My Settings</a>
+                <a class="collapse-item" href="payments.php">My Payments</a>
+                <a class="collapse-item" href="">Contact Us</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Addons
+    </div>
+
+
+    <!-- Nav Item - Users -->
+    <li class="nav-item">
+        <a class="nav-link" href="applicants.php">
+            <i class="fas fa-fw fa-id-card"></i>
+            <span>Applicants</span></a>
+    </li>
+
+    <!-- Nav Item - Jobs -->
+    <li class="nav-item">
+        <a class="nav-link" href="postings.php">
+            <i class="fas fa-fw fa-sitemap"></i>
+            <span>Postings</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
